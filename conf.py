69e3d930b41bce6a60ddac54c73461c1a7e8a440
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2026-03-15 17:03:16 +0100

    handle command checks better, add !emotes command (debug), make neuro color a little bit darker

--- conf.py
+++ conf.py
@@ -1,5 +1,8 @@
 # Channels that the !randomsong is allowed in
 ALLOWED_CHANNELS = (0,)
+# Max songs cached, since we use double cache, with requested songs it will be 8
+# (4 in random queue and 4 in request queue, assuming there is 4 or more songs requested)
+MAX_CACHE = 4
 
 from enum import Enum, IntEnum
 
@@ -8,7 +11,7 @@ from enum import Enum, IntEnum
 class COLORS(IntEnum):
     QUEUE = 0x237FEB
     EMBED_DEFAULT = 0x237FEB
-    NEURO = 0xFDE7D3
+    NEURO = 0xFBD1A9
     EVIL = 0x8F0A0A
     VEDAL = 0x0A7908
 
